c77137e153f7c84c0e83e35d8b4bc767aac50753
Adam Rowell <k473th@gmail.com>
2015-05-28 22:19:52 +0100

    WhisperPowerOptions

--- Button.js
+++ Button.js
@@ -13,11 +13,11 @@ on("chat:message", function (msg) {
             name = args.shift(),
             targets = args.shift(),
             targetlist = args.join(" ");
-        
+
         var attrs = findObjs({characterid:charid, name:name}),
             newMsg = _.clone(msg),
             player_obj = getObj("player", msg.playerid);
-        
+
         if (attrs[0]){
             newMsg.content = attrs[0].get("current").replace('?{Number of Targets|1}', targets) + targetlist;
             sendChat('', newMsg.content, function(inner){
@@ -30,26 +30,27 @@ on("chat:message", function (msg) {
         var args = content.split(" ");
         args.shift();
         var charId = args.shift(),
-            targets = args.shift(),
+            t = args.shift(),
+            targets = t ? Number(t) : 0,
             weapons = args.join(' ').split(' --'),
             power = weapons.shift(),
-            mark = '!mark ' + _.times(Number(targets), function(i){
+            mark = '!mark ' + _.times(targets, function(i){
                 return '@{target|Target ' + (i + 1) + '|token_id}';
             }).join(' '),
-            targetList = ' --target_list|' + _.times(Number(targets), function(i){
+            targetList = ' --target_list|' + _.times(targets, function(i){
                 return '@{target|Target ' + (i + 1) + '|token_id}';
             }).join(' | '),
             message = _.chain(weapons)
                 .map(function(w){
                     var a = w.split('|');
-                    return '[' + a[0] + '](!button ' + charId + ' ' + a[1] + ' ' + targets + ' #target)';
+                    return '[' + a[0] + '](!button ' + charId + ' ' + a[1] + ' ' + String(targets) + ' #target)';
                 }).reduce(function(a, b) {
                     return a + b;
                 }, '/w ' + msg.who.split(' ')[0] + ' ')
                 .value();
         sendChat(power, message);
-            
-        findObjs({_type: "macro", name: "target"}, function(obj){obj.remove()});
+
+        _.map(findObjs({_type: "macro", name: "target"}), function(obj){obj.remove()});
         createObj("macro", {
             name: "target",
             action: targetList + "\n" + mark,
@@ -57,4 +58,49 @@ on("chat:message", function (msg) {
             playerid: msg.playerid
         });
     }
-});    
+});
+
+ch = function (c) {
+	var entities = {
+		'<' : 'lt',
+		'>' : 'gt',
+		"'" : '#39',
+		'@' : '#64',
+		'{' : '#123',
+		'|' : '#124',
+		'}' : '#125',
+		'[' : '#91',
+        '(' : '#40',
+		']' : '#93',
+        ')' : '#41',
+		'"' : 'quot'
+	};
+
+	if(_.has(entities,c) ){
+		return ('&'+entities[c]+';');
+	} else {
+        return c;
+    }
+}
+
+function WhisperPowerOptions(who, what){
+    var Actions = _.chain(filterObjs(function(obj) {
+            if (obj.get('characterid') !== what) return false;
+            if (obj.get('type') !== 'attribute') return false;
+            return obj.get('name').match(/repeating_powers_\d+_action/);
+        }))
+        .map(function(o){return {name:o.get('name').match(/\d+/)[0], current:o.get('current').replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();})}})
+        .groupBy('current')
+        .value(),
+        Player = who.get('displayname').split(' ')[0];
+    _.each(Actions, function(Powers, Title){
+        var names = _.pluck(Powers, 'name'),
+            displays = _.map(names, function(i) {
+                var name = getAttrByName(what, 'repeating_powers_' + i + '_name'),
+                    macro = getAttrByName(what, 'repeating_powers_' + i + '_macro');
+                return '{{[' + name + '](' + _.map(macro, ch).join('') + ')}}'
+                }).join(' '),
+            power = '/w ' + Player + ' &{template:default} {{name=' + Title + '}} ' + displays;
+        sendChat('Powers', power);
+    });
+}
